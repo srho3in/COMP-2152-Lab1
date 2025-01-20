30c718fc24903b7a07d96ecc4a5b48017856274c
MrKhan <152137159+srho3in@users.noreply.github.com>
2025-01-19 23:25:31 -0500

    Initial commit for lab1

---
+++ lab1.py
@@ -0,0 +1,15 @@
+# Hossein Khanzadeh
+
+# Defining an array variable
+numbers = [1, 4, 7, 9]
+
+# Defining variables
+a, b, c, d = 1, 2, 3, 4
+
+# Order of Operations
+e = (a * c) - (b / d)
+
+e = (a - ((b ** c) // d) + (a % c))
+
+print("Array:", numbers)
+print("Result of first equation:", e)
